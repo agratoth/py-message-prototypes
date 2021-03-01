7c2d663d7bbf2a30dfcb9e1df2d5dd903ae854a8
Anton Berdnikov <agratoth@yandex.ru>
2021-03-01 11:04:14 +0400

    [0.2.1] Add AuthMessage (for authorization flows)

--- message_prototypes/pipe_messages/__init__.py
+++ message_prototypes/pipe_messages/__init__.py
@@ -2,6 +2,7 @@ from .action_path_message import ActionPathMessage
 from .request_extra_message import RequestExtraMessage
 from .request_message import RequestMessage
 from .response_message import ResponseMessage
+from .auth_message import AuthMessage
 
 
 __all__ = (
@@ -9,4 +10,5 @@ __all__ = (
     'RequestExtraMessage',
     'RequestMessage',
     'ResponseMessage',
+    'AuthMessage',
 )
